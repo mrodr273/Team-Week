33aa432de05fcee6bf5e0b4376306dee26f98cf6
neilbateman <46337141+neilbateman@users.noreply.github.com>
2019-04-03 19:52:43 -0700

    changes to html tags and script for player object

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
 //Business Logic for AddressBook -----------
-var eventBook = new EventBook();
+
 
 function EventBook() {
   this.event = [];
 }
 
+var eventBook = new EventBook();
+
+function playerEventBook() {
+  this.playerEvent = [];
+}
+
+var playerEventBook = new playerEventBook();
+
+
+
 EventBook.prototype.addEvent = function(info) {
   this.event.push(info);
+}
+
+playerEventBook.prototype.addPlayerEvent = function(x) {
+  this.playerEvent.push(x);
 }
 
 
@@ -25,7 +39,16 @@ function Event(title, weight, min, max, where, when, time) {
   this.when = when;
   this.time = time;
 }
-
+function playerEvent(titlePlayer, weightPlayer, minPlayer, maxPlayer, wherePlayer, whenPlayer, timePlayer) {
+  this.titlePlayer  = titlePlayer;
+  this.weightPlayer = weightPlayer;
+  this.minPlayer = minPlayer;
+  this.maxPlayer = maxPlayer;
+  this.wherePlayer = wherePlayer;
+  this.whenPlayer = whenPlayer;
+  this.timePlayer = timePlayer;
+}
+//Can break into 2 functions or leave as one
 function resetFields() {
     $("input#name").val("");
     $("input#title").val("");
@@ -35,9 +58,19 @@ function resetFields() {
     $("input.where").val("");
     $("input#when").val("");
     $("input.time").val("");
+    //Section for reseting player inputs
+    //Add or delete inputs
+    $("input#namePlayer").val("");
+    $("input#titlePlayer").val("");
+    $("input.weightPlayer").val("");
+    $("input#minPlayer").val("");
+    $("input#maxPlayer").val("");
+    $("input.wherePlayer").val("");
+    $("input#whenPlayer").val("");
+    $("input.timePlayer").val("");
 }
 
-
+  //Might not need it for player unless we want to confirm inputs
  var inputEvent = function(eventBook) {
    $("ul#eventDetails").text("");
    for (var index=0; index < eventBook.event.length; index++) {
@@ -55,11 +88,40 @@ function resetFields() {
    };
  };
 
+ var inputPlayerEvent = function(playerEventBook) {
+   $("ul#playerDetails").text("");
+   for (var index=0; index < playerEventBook.playerEvent.length; index++) {
+     //debugger;
+     $("ul#playerDetails").append("<li> " + playerEventBook.playerEvent[index].titlePlayer + "</li>"); $("ul#eventDetails").append("<li> " + eventBook.event[index].weight + "</li>");
+     $("ul#playerDetails").append("<li> " + playerEventBook.playerEvent[index].minPlayer + "</li>");
+     $("ul#playerDetails").append("<li> " + playerEventBook.playerEvent[index].maxPlayer + "</li>");
+     $("ul#playerDetails").append("<li> " + playerEventBook.playerEvent[index].wherePlayer + "</li>");
+     $("ul#playertDetails").append("<li> " + playerEventBook.playerEvent[index].whenPlayer + "</li>");
+     $("ul#playerDetails").append("<li> " + playerEventBook.playerEvent[index].time + "</li>");
+     // addressBook.contacts[index].addresses.forEach(function(address) {
+     //   $("ul#eventDetails").append("<li>" + address.type + ": " + address.fullAddress() + "</li>");
+
+
+   };
+ };
+
 
 //User Interface Logic -----------
 $(document).ready(function() {
+
+  $("#playerbtn").click(function(event) {
+    $("#player").show();
+    $("#creator").hide();
+  });
+
+  $("#creatorbtn").click(function(event) {
+    $("#creator").show();
+    $("#player").hide();
+  });
+
+  //Form for Creator
   $("#game").submit(function(event) {
-    debugger;
+    // debugger;
   var creatortNameInput = $("input#name").val();
   var creatorTitleInput = $("input#title").val();
   var creatorWeightInput = $("input:radio[name=weight]:checked").val();
@@ -70,29 +132,34 @@ $(document).ready(function() {
   var creatorTimeInput = $("input:checkbox[name=time]:checked").val();
 
   var creatorEvent = new Event(creatorTitleInput, creatorWeightInput, creatorMinInput, creatorMaxInput, creatorWhereInput, creatorWhenInput, creatorTimeInput);
+  console.log(creatorEvent);
   eventBook.addEvent(creatorEvent);
-// debugger;
-console.log(eventBook);
   inputEvent(eventBook);
-  //$("#eventDetails").show();
-  // $(".new-address").each(function() {
-  //   debugger;
-  //   var addressType = $(this).find("select.address-type").val();
-  //   var inputtedStreet = $(this).find("input.new-street").val();
-  //   var inputtedCity = $(this).find("input.new-city").val();
-  //   var inputtedState = $(this).find("input.new-state").val();
-  //   var newAddress = new Address(addressType, inputtedStreet, inputtedCity, inputtedState);
-  //   newContact.addresses.push(newAddress);
-  // });
-  //
-   resetFields();
-  //
-  // addressBook.addContact(newContact);
-  // inputContact(addressBook);
-
+  resetFields();
 
   event.preventDefault();
-  // });
+  });
 
+  //Form for Player
+  $("#gamePlayer").submit(function(event) {
+    // debugger;
+  var PlayerNameInput = $("input#namePlayer").val();
+  var PlayerTitleInput = $("input#titlePlayer").val();
+  var PlayerWeightInput = $("input:radio[name=weightPlayer]:checked").val();
+  var PlayerMinInput = $("input#minPlayer").val();
+  var PlayerMaxInput = $("input#maxPlayer").val();
+  var PlayerWhereInput = $("input:checkbox[name=wherePlayer]:checked").val();
+  var PlayerWhenInput = $("#whenPlayer").val();
+  var PlayerTimeInput = $("input:checkbox[name=timePlayer]:checked").val();
+
+
+  var newPlayerEvent = new playerEvent(PlayerTitleInput, PlayerWeightInput, PlayerMinInput, PlayerMaxInput, PlayerWhereInput, PlayerWhenInput, PlayerTimeInput);
+  console.log(newPlayerEvent);
+  playerEventBook.addPlayerEvent(newPlayerEvent);
+  console.log(playerEventBook);
+  // resetFields();
+  inputPlayerEvent(newPlayerBook);
+
+  event.preventDefault();
   });
 });
